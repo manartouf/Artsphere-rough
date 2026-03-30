7e139ccc95bcf15af5cf5680452bd5cfc9b8a291
manartouf <manartchy@gmail.com>
2026-03-31 02:48:33 +0600

    Fix axiosConfig,js fault

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// This tells the app to use the live Render URL if it exists, otherwise use localhost
+const API_URL = import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : 'http://localhost:5000/api';
+
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api', 
+    baseURL: API_URL, 
 });
 
 // This piece of code "intercepts" every request and adds the token
